bb28940ed5bb12175359f14224a59756b28cf1ff
Rohit <rohitmondall8000@gmail.com>
2023-06-04 19:13:33 +0530

    completed contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
         <div className='bg-zinc-300 py-20'>
             <h1 className='text-center text-5xl font-bold'>Contact Us</h1>
 
-            <div className=''>
+            <div className='flex flex-row justify-evenly items-center mt-20'>
                 <div className='contact_form'>
                     <Box maw={400} mx="auto">
                         <form onSubmit={form.onSubmit((values) => console.log(values))}>
@@ -57,10 +57,13 @@ const Contact = () => {
                                 {...form.getInputProps('termsOfService', { type: 'checkbox' })}
                             />
 
-                            <Group position="right" mt="md">
+                            <Group
+                                position="center"
+                                mt="md"
+                            >
                                 <Button
                                     type="submit"
-                                    className='bg-sky-500 transition-all duration-300'
+                                    className='text-lg bg-sky-500 transition-all duration-300 my-5 hover:scale-110 active:scale-90'
                                 >
                                     Submit
                                 </Button>
@@ -69,8 +72,30 @@ const Contact = () => {
                     </Box>
                 </div>
 
-                <div className='contact_address'>
-                    
+                <div className='contact_address bg-white p-10'>
+                    <h1 className='text-4xl font-bold mb-10'>Contact Information</h1>
+
+                    <div className='address'>
+                        <h4>Address</h4>
+                        <p className='first-child'>717 Market Street</p>
+                        <p>San Fransisco, CA 94103</p>
+                    </div>
+
+                    <div className='phn'>
+                        <h4>Phone Number</h4>
+                        <p className='first-child'>+91-994XXXXXXX</p>
+                    </div>
+
+                    <div>
+                        <h4>Email</h4>
+                        <p
+                            href=''
+                            target='_blank'
+                            className='underline'
+                        >
+                            company.estate@gmail.com
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
